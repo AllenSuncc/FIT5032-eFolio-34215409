e5e285e27182d60dffdc7f32dd7640d3bcca212f
Haojun Sun <hsun0066@student.monash.edu>
2025-08-19 21:02:33 +1000

    Complete Activity 1

---
+++ Week3/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Form from './components/Form.vue'
+</script>
+
+<template>
+  <div>
+    <Form />
+  </div>
+</template>
+
+<style scoped>
+// Our logic will go here
+</style>
