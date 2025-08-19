d7b54842c1d2e8ed20d0537f180bc1ac4a730f6c
Haojun Sun <hsun0066@student.monash.edu>
2025-08-19 22:49:48 +1000

    Complete Activity 6

--- Week3/src/components/Form.vue
+++ Week3/src/components/Form.vue
@@ -79,16 +79,37 @@
             </button>
           </div>
         </form>
+
+        <div class="row mt-5" v-if="submittedCards.length">
+          <div class="d-flex flex-wrap justify-content-start">
+            <div 
+              v-for="(card, index) in submittedCards" 
+              :key="index" 
+              class="card m-2" 
+              style="width: 18rem;"
+            >
+              <div class="card-header">
+                User Information
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Username: {{ card.username }}</li>
+                <li class="list-group-item">Password: {{ card.password }}</li>
+                <li class="list-group-item">Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}</li>
+                <li class="list-group-item">Gender: {{ card.gender }}</li>
+                <li class="list-group-item">Reason: {{ card.reason }}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { ref } from 'vue'
 
-
-const formData = reactive({
+const formData = ref({
   username: '',
   password: '',
   isAustralian: false,
@@ -97,20 +118,41 @@ const formData = reactive({
 })
 
 
+const submittedCards = ref([])
+
+
 function submitForm() {
-  console.log('Form Submitted:', formData)
+  submittedCards.value.push({
+    ...formData.value
+  })
+  clearForm() 
 }
 
 
 function clearForm() {
-  formData.username = ''
-  formData.password = ''
-  formData.isAustralian = false
-  formData.gender = ''
-  formData.reason = ''
+  formData.value = {
+    username: '',
+    password: '',
+    isAustralian: false,
+    gender: '',
+    reason: ''
+  }
 }
 </script>
 
 <style scoped>
-
+.card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.card-header {
+  background-color: #275FDA;
+  color: white;
+  padding: 10px;
+  border-radius: 10px 10px 0 0;
+}
+.list-group-item {
+  padding: 10px;
+}
 </style>
