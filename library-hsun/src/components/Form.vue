6c0b4dab6d6817257a49ede4f99d9c53f344408c
Haojun Sun <hsun0066@student.monash.edu>
2025-08-19 23:46:54 +1000

    Complete Week4 Activity 2

--- library-hsun/src/components/Form.vue
+++ library-hsun/src/components/Form.vue
@@ -5,38 +5,22 @@
         <h1 class="text-center">User Information Form</h1>
         <form @submit.prevent="submitForm">
 
-          <!-- 用户名 + 密码 -->
           <div class="row mb-3">
             <div class="col-6 col-md-6">
               <label for="username" class="form-label">Username</label>
-              <input 
-                type="text" 
-                class="form-control" 
-                id="username" 
-                v-model="formData.username"
-              >
+              <input type="text" class="form-control" id="username" required v-model="formData.username" />
             </div>
             <div class="col-6 col-md-6">
               <label for="password" class="form-label">Password</label>
-              <input 
-                type="password" 
-                class="form-control" 
-                id="password" 
-                v-model="formData.password"
-              >
+              <input type="password" class="form-control" id="password" minlength="4" maxlength="10"
+                v-model="formData.password" />
             </div>
           </div>
 
-          <!-- Australian Resident? + Gender -->
           <div class="row mb-3">
             <div class="col-6 col-md-6">
               <div class="form-check">
-                <input 
-                  type="checkbox" 
-                  class="form-check-input" 
-                  id="isAustralian" 
-                  v-model="formData.isAustralian"
-                >
+                <input type="checkbox" class="form-check-input" id="isAustralian" v-model="formData.isAustralian">
                 <label class="form-check-label" for="isAustralian">
                   Australian Resident?
                 </label>
@@ -44,11 +28,7 @@
             </div>
             <div class="col-6 col-md-6">
               <label for="gender" class="form-label">Gender</label>
-              <select 
-                class="form-select" 
-                id="gender" 
-                v-model="formData.gender"
-              >
+              <select class="form-select" id="gender" v-model="formData.gender">
                 <option disabled value="">Please select</option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
@@ -57,39 +37,22 @@
             </div>
           </div>
 
-          <!-- Reason for joining -->
           <div class="mb-3">
             <label for="reason" class="form-label">Reason for joining</label>
-            <textarea 
-              class="form-control" 
-              id="reason" 
-              rows="3" 
-              v-model="formData.reason"
-            ></textarea>
+            <textarea class="form-control" id="reason" rows="3" v-model="formData.reason"></textarea>
           </div>
 
-          <!-- 提交和清空按钮 -->
           <div class="text-center">
             <button type="submit" class="btn btn-primary me-2">Submit</button>
-            <button 
-              type="button" 
-              class="btn btn-secondary" 
-              @click="clearForm"
-            >
+            <button type="button" class="btn btn-secondary" @click="clearForm">
               Clear
             </button>
           </div>
         </form>
 
-        <!-- 横向排列的用户信息卡片 -->
         <div class="mt-5" v-if="submittedCards.length">
           <div class="d-flex flex-wrap justify-content-center">
-            <div 
-              v-for="(card, index) in submittedCards" 
-              :key="index" 
-              class="card m-2" 
-              style="width: 18rem;"
-            >
+            <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem;">
               <div class="card-header">
                 User Information
               </div>
@@ -125,7 +88,7 @@ function submitForm() {
   submittedCards.value.push({
     ...formData.value
   })
-  clearForm() 
+  clearForm()
 }
 
 function clearForm() {
@@ -145,12 +108,14 @@ function clearForm() {
   border-radius: 10px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
+
 .card-header {
   background-color: #275FDA;
   color: white;
   padding: 10px;
   border-radius: 10px 10px 0 0;
 }
+
 .list-group-item {
   padding: 10px;
 }
