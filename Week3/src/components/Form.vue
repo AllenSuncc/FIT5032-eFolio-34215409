67cac76b2242c929ea9302d4ac2b9f726cb564d3
Haojun Sun <hsun0066@student.monash.edu>
2025-08-19 22:14:45 +1000

    Add the username input field for Activity 5

--- Week3/src/components/Form.vue
+++ Week3/src/components/Form.vue
@@ -3,15 +3,38 @@
     <div class="row">
       <div class="col-md-8 offset-md-2">
         <h1 class="text-center">User Information Form</h1>
+        <form @submit.prevent="submitForm">
+          <div class="row mb-3">
+            <div class="col-md-6">
+              <label for="username" class="form-label">Username</label>
+              <input 
+                type="text" 
+                class="form-control" 
+                id="username" 
+                v-model="formData.username"
+              >
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-// Our logic will go here
+import { reactive } from 'vue'
+
+
+const formData = reactive({
+  username: ''
+})
+
+
+function submitForm() {
+  console.log(formData)
+}
 </script>
 
 <style scoped>
-/* Our styles will go here */
+
 </style>
