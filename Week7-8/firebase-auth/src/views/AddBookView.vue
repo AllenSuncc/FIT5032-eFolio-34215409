f7d37ce77cb088dbe5db6a4c9a8b5a188fc8802d
Haojun Sun <hsun0066@student.monash.edu>
2025-09-18 16:25:12 +1000

    complete template part of AddBookView

---
+++ Week7-8/firebase-auth/src/views/AddBookView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Add Book</h1>
+    <form @submit.prevent="addBook">
+      <div>
+        <label for="isbn">ISBN:</label>
+        <input type="text" v-model="isbn" id="isbn" required />
+      </div>
+      <div>
+        <label for="name">Name:</label>
+        <input type="text" v-model="name" id="name" required />
+      </div>
+      <button type="submit">Add Book</button>
+    </form>
+  </div>
+</template>
